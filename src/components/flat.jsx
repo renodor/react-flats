83508f936a4195e9dae35cbed5e4f345db075a83
renaud dor <renaud.dor@gmail.com>
2020-07-31 12:06:41 +0200

    add props to flat_list and flat components

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 
 class Flat extends Component {
-
   render() {
-
+    const url = 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg';
     const flatStyle = {
-      backgroundImage: "url('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg')"
-    }
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('${this.props.imageUrl}')`
+    };
 
     return (
       <div className="card" style={flatStyle}>
-        <div className="card-category">150 EUR</div>
+        <div className="card-category">{this.props.price} {this.props.priceCurrency}</div>
         <div className="card-description">
-          <h2>Super 60m2 in trendy neighborhood!</h2>
+          <h2>{this.props.name}</h2>
         </div>
         <a className="card-link" href="#"></a>
       </div>
--- src/components/flat_list.jsx
+++ src/components/flat_list.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Flat from '../components/flat';
+import Flat from './flat';
 
 import flats from '../../data/flats';
 
@@ -9,7 +9,15 @@ class FlatList extends Component {
   render() {
     return (
       <div className="flat-list">
-        <Flat />
+        {flats.map((flat, i) => {
+          return <Flat
+            key={i}
+            price={flat.price}
+            priceCurrency={flat.priceCurrency}
+            name={flat.name}
+            imageUrl={flat.imageUrl} />
+        })
+      }
       </div>
     );
   }
