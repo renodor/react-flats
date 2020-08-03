801daa892feff5cc231413816177af5f89055ffc
renaud dor <renaud.dor@gmail.com>
2020-08-03 12:03:43 +0200

    center map on paris by default

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -7,16 +7,16 @@ import Marker from './marker'
 class SimpleMap extends Component {
   static defaultProps = {
     center: {
-      lat: 59.95,
-      lng: 30.33
+      lat: 48.856946,
+      lng: 2.342124
     },
-    zoom: 11
+    zoom: 13
   };
 
   render() {
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
+      <div className="map-container" style={{ height: '100vh' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyA9eczdhAXBjQXg_Y5E8niGj9bVsr237OM' }}
           defaultCenter={this.props.center}
