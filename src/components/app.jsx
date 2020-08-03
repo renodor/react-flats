ea2e1d4ced48078050d4b518d4a38e1eab3ada05
renaud dor <renaud.dor@gmail.com>
2020-08-03 12:18:33 +0200

    start redesigning/improving app with correction

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,32 +1,45 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
+import flats from '../../data/flats';
 import FlatList from './flat_list';
-import SimpleMap from './map';
+import Marker from './marker';
+
 
 class App extends Component {
-
   constructor(props) {
     super(props);
 
-
     this.state = {
-      markerLat: 43.1487189,
-      markerLng: 5.7420883
+      selectedFlat: flat[0],
+      flats
     };
   }
 
-  selectFlat = (lat, lng) => {
-    this.setState({
-      markerLat: lat,
-      markerLng: lng
-    });
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
   }
 
   render() {
     return (
       <div>
         <FlatList selectFlat={this.selectFlat} />
-        <SimpleMap markerLat={this.state.markerLat} markerLng={this.state.markerLng} />
+        <div className="map-container" style={{ height: '100vh' }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: 'AIzaSyA9eczdhAXBjQXg_Y5E8niGj9bVsr237OM' }}
+            defaultCenter={this.center()}
+            defaultZoom={12}
+          >
+            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+          </GoogleMapReact>
+        </div>
       </div>
     );
   }
