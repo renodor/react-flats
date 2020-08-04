02c18b68c1cd92f22aee0a7076456e25c51e7e8f
renaud dor <renaud.dor@gmail.com>
2020-08-04 10:54:21 +0200

    solve offenses on flat.jsx

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -2,21 +2,27 @@ import React, { Component } from 'react';
 
 class Flat extends Component {
   handleClick = () => {
-    this.props.selectFlat(this.props.index);
+    const { index, selectFlat } = this.props;
+    selectFlat(index);
   }
 
   render() {
+    const { flat, selectedFlat } = this.props;
     const flatStyle = {
-      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('${this.props.flat.imageUrl}')`
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('${flat.imageUrl}')`
     };
 
     return (
-      <div className={`card${this.props.selectedFlat ? ' active' : ''}`} style={flatStyle}>
-        <div className="card-category">{this.props.flat.price} {this.props.flat.priceCurrency}</div>
-        <div className="card-description">
-          <h2>{this.props.flat.name}</h2>
+      <div className={`card${selectedFlat ? ' active' : ''}`} style={flatStyle}>
+        <div className="card-category">
+          {flat.price}
+          {' '}
+          {flat.priceCurrency}
         </div>
-        <a className="card-link" href="#" onClick={this.handleClick}></a>
+        <div className="card-description">
+          <h2>{flat.name}</h2>
+        </div>
+        <button type="button" className="card-link" href="#" onClick={this.handleClick} />
       </div>
     );
   }
